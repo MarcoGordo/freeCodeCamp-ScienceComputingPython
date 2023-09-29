ee4dc39d329866a0bbcb876138edd1dd338e67ea
MarcoGordo <mapulido1989@gmail.com>
2023-09-29 13:11:33 -0400

    This is the first part of the version with out regex

--- arithmeticArranger.py
+++ arithmeticArranger.py
@@ -1,4 +1,23 @@
 # Aritmetic Arranger
 
-def arithmetic_arranger(arrayOfOps , myBool):
-    print("Hello")
+def arithmetic_arranger(arrayOfOps, myBool):
+    operations = ['+', '-', '*', '/']
+    part1 = ""
+    part2 = ""
+    leosona = ""
+    mySignal = True
+    for element in arrayOfOps:
+        for char in element:
+            if char.isnumeric() and mySignal:
+                part1 += char
+            elif char in operations:
+                leosona = char
+                mySignal = False
+            elif char.isnumeric() and not mySignal:
+                part2 += char
+                mySignal = False  
+    print(part1)
+    print(leosona)
+    print(part2)
+
+arithmetic_arranger(['45+26'], True)
