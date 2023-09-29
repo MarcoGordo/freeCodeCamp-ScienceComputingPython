17437922989b4e84f3a8992614ec9301084ca052
MarcoGordo <mapulido1989@gmail.com>
2023-09-29 15:26:37 -0400

    This code is working, ie, it gets the aim of the project

--- arithmeticArranger.py
+++ arithmeticArranger.py
@@ -2,10 +2,13 @@
 
 def arithmetic_arranger(arrayOfOps, myBool):
     operations = ['+', '-', '*', '/']
-    part1 = ""
-    part2 = ""
-    leosona = ""
+    part1 = ''
+    part2 = ''
+    leosona = ''
+    whiteSpaces = 1
+    i = 0
     mySignal = True
+    #spliting the strings
     for element in arrayOfOps:
         for char in element:
             if char.isnumeric() and mySignal:
@@ -15,10 +18,24 @@ def arithmetic_arranger(arrayOfOps, myBool):
                 mySignal = False
             elif char.isnumeric() and not mySignal:
                 part2 += char
-                mySignal = False  
-    print(part1)
-    print(leosona)
-    print(part2)
-    print('ok')
-
-arithmetic_arranger(['45+26'], True)
+    # Adding the Format            
+    whiteSpaces = abs(len(part1) - len(part2))           
+    if len(part1) > len(part2):
+        while(i < whiteSpaces):
+            part2 = ' ' + part2  
+            i +=1
+    else:
+        while(i < whiteSpaces):
+            part1 = ' ' + part1  
+            i += 1                  
+    toPrint = [ ' ' + part1 , leosona + part2]
+    for element in toPrint:
+        print(element)
+    for char in toPrint[1] :
+        print('-' , end='')
+    print()
+    if myBool == True :
+        print(' ' + str(eval(arrayOfOps[0][0:len(arrayOfOps)-2])))
+        
+        
+arithmetic_arranger(['4505 * 26'], False)
