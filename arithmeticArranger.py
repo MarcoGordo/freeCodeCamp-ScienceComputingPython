b23aa1466000d617d790ec72aff28426cb01271f
MarcoGordo <mapulido1989@gmail.com>
2023-10-03 18:17:22 -0400

    first functional version

--- arithmeticArranger.py
+++ arithmeticArranger.py
@@ -1,19 +1,36 @@
-# Aritmetic Arranger
 
-def arithmetic_arranger(problems):
-    base_line = ''
-    if(len(problems)) > 5:
+def arithmetic_arranger(problems , solve = False):
+    # First check, Length of the list is almost of five problems
+    if len(problems) > 5:
         return 'Error: Too many problems.'
-    arranged_problems = [problems[0], problems[1], problems[2]] 
-    firstProblem = problems[0].split()
-    print(firstProblem)
-    if(len(firstProblem[0]) >= len(firstProblem[2])):
-        for char in ' ' + firstProblem[0] : 
-            base_line += '-'
-    else:
-        for char in ' ' + firstProblem[2] : 
-            base_line += '-'    
-    arranged_problem  = ' ' + firstProblem[0] + '\n' + firstProblem[1] + ' ' + firstProblem[2] + '\n' + base_line
-    print(arranged_problem)
-    return arranged_problems 
-print(arithmetic_arranger(['235 + 52','11 + 3' , '5 + 1002']))
+
+    top_line = ''
+    middle_line = ''
+    bottom_line = ''
+    result_line = ''
+
+    for problem in problems:
+        firstProblem = problem.split()
+        operand1 = firstProblem[0]
+        operator = firstProblem[1]
+        operand2 = firstProblem[2]
+
+        max_length = max(len(operand1), len(operand2)) + 2
+        top_line += operand1.rjust(max_length) + '    '
+        middle_line += operator + operand2.rjust(max_length - 1) + '    '
+        bottom_line += '-' * max_length + '    '
+
+        if(solve == True):
+            try:
+                result = str(eval(problem))
+                result_line += result.rjust(max_length) + '    '
+            except:
+                return 'Error: Invalid problem.'
+
+    arranged_problems = top_line.rstrip() + '\n' + middle_line.rstrip() + '\n' + bottom_line.rstrip()
+    if result_line:
+        arranged_problems += '\n' + result_line.rstrip()
+
+    return arranged_problems
+
+print(arithmetic_arranger(['235 + 52', '11 + 3', '1002 + 5', '1002 + 6', '5024 + 1002'], True))
