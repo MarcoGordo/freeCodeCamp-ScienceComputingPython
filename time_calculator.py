55351901799a0390bb28c4dc295cc57e776dfa75
MarcoGordo <mapulido1989@gmail.com>
2023-10-13 18:44:26 -0400

    This is the third part of the program

--- time_calculator.py
+++ time_calculator.py
@@ -1,5 +1,6 @@
 
 def add_time(start_time , duration_time,  day='Monday'):
+    string_for_back = ""
     step_one = start_time.split(" ")
     start_time_div = step_one[0].split(":")
     duration_time_div = duration_time.split(":")
@@ -7,8 +8,11 @@ def add_time(start_time , duration_time,  day='Monday'):
     total_of_minutes = int(start_time_div[1]) + int(duration_time_div[1])
     total_of_days = total_of_hours/24
     if total_of_minutes/60 > 1:
-        total_of_hours += total_of_minutes//60    
-    return total_of_hours
+        total_of_hours += total_of_minutes//60
+        total_of_minutes -= (total_of_minutes//60)*60 
+        string_for_back = str(total_of_hours) + ":" + str(total_of_minutes)
+        return string_for_back     
+    return total_of_minutes
 
 
 print(add_time("3:25 PM","6:45")) 
