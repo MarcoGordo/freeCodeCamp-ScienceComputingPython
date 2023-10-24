8654803cde660bf2b45968f11f6287a24f55e45d
MarcoGordo <mapulido1989@gmail.com>
2023-10-24 11:03:12 -0400

    this file has the correction for the time convertion

--- time_calculator.py
+++ time_calculator.py
@@ -9,17 +9,22 @@ def add_time(start_time , duration_time,  day='Monday'):
     total_of_hours = int(start_time_div[0]) + int(duration_time_div[0]) 
     total_of_minutes = int(start_time_div[1]) + int(duration_time_div[1])
     #number of days that exist in the information
-    total_of_days = total_of_hours/24
+    total_of_days_after = total_of_hours//24 
+    total_of_hours -= total_of_days_after * 24 
+    if total_of_hours > 12:
+        total_of_hours -= 12
     #working the strings
+    
     if total_of_minutes/60 > 1:
-        total_of_hours += total_of_minutes//60
-        total_of_minutes -= (total_of_minutes//60)*60 
+        total_of_hours += total_of_minutes//60 
+        total_of_minutes -= (total_of_minutes//60)*60    
         string_for_back = str(total_of_hours) + ":" + str(total_of_minutes)
         return string_for_back
+    
     else:
-        string_for_back = str(total_of_hours) + ":" + str(total_of_minutes)
+        string_for_back = str(total_of_hours) + ":" + str(total_of_minutes) + " " + step_one[1]
         return string_for_back         
     
 
 
-print(add_time("3:25 PM","6:05")) 
+print(add_time("6:30 PM","205:12")) 
